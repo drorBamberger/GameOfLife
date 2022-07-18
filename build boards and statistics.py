24f72056684c50d597ebeb4d78124acb243f5bb5
drorBamberger <drorbamberger@gmail.com>
2022-07-18 19:34:50 +0300

    Split file functions

--- build boards and statistics.py
+++ build boards and statistics.py
@@ -1,92 +1,9 @@
-import numpy as np
-import time
-import matplotlib.pyplot as plt
+from functions import *
 
-start = time.time()
-SIZE = 20
-
-M = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
-
-
-# the calcUneighs, make_move and generate_population functions from:
-# https://medium.com/@ptyshevs/rgol-ga-1cafc67db6c7
-
-def calc_neighs(field, i, j):
-    """ Calculate number of neighbors alive (assuming square field) """
-    neighs = 0
-    n = len(field)
-    for m in M:
-        row_idx = m[0] + i
-        col_idx = m[1] + j
-        if 0 <= row_idx < n and 0 <= col_idx < n:
-            if field[row_idx][col_idx]:
-                neighs += 1
-    return neighs
-
-
-def make_move(field, moves=1):
-    """ Make a move forward according to Game of Life rules """
-    n = len(field)
-    cur_field = field
-    for _ in range(moves):
-        new_field = np.zeros((n, n), dtype='uint8')
-        for i in range(n):
-            for j in range(n):
-                neighs = calc_neighs(cur_field, i, j)
-                if cur_field[i][j] and neighs == 2:
-                    new_field[i][j] = 1
-                if neighs == 3:
-                    new_field[i][j] = 1
-        cur_field = new_field
-    return cur_field
-
-
-def generate_population(amount, random_state=-1):
-    """
-    Generating initial population of individual solutions
-    :return: initial population as a list of 20x20 arrays
-    """
-    if random_state != -1:
-        np.random.seed(random_state)
-    initial_states = np.split(np.random.binomial(1, 0.5, (SIZE * amount, SIZE)).astype('uint8'), amount)
-    return deduction_edges(make_move(state, 0) for state in initial_states)
-
-
-def deduction_edges(l):
-    for i in l:
-        if sum(sum(i)) < SIZE * SIZE * 0.05:
-            return []
-        elif sum(sum(i)) > SIZE * SIZE * 0.95:
-            return []
-        else:
-            return i
-
-
-def Average(l):
-    avg = sum(l) / len(l)
-    return avg
-
-
-def min_no_zero(arr):
-    for i in range(len(arr)):
-        if arr[i] != 0:
-            return i
-    return -1
-
-
-def max_no_zero(arr):
-    for i in range(len(arr)):
-        if arr[-i] != 0:
-            return len(arr) - i
-    return -1
-
-
-amount = 1000000
-
-# stack,CL,CH = generate_population(amount)
+START = time.time()
 
 stack = []
-for i in range(amount):
+for i in range(AMOUNT):
     # print(generate_population(1, i))
     stack.append(generate_population(1, 100 * i))
 
@@ -109,6 +26,6 @@ plt.legend()
 
 plt.show()
 
-end = time.time()
+END = time.time()
 
-print("the running take:", end - start, "sec")
+print("the running take:", END - START, "sec")
